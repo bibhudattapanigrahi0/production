56a1bcd0e3be4287b5e1786e849f0beef6768601
bibhudatta <bibhudattapanigrahi0@gmail.com>
2020-05-01 20:09:53 +0530

    added selection sort functionality

--- sort.cpp
+++ sort.cpp
@@ -1,11 +1,39 @@
 #include<stdio.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+vector<int> slection_sort(vector<int> a){
+int n=a.size();
+for(int i=0;i<n-1;i++){
+int x=i;
+for(int j=i;j<n;j++){
+if(a[x]>a[j])
+	x=j;
 
+}
+swap(a[i],a[x]);
+
+
+}
+
+return a;
+
+
+
+}
 
 int main(){
 
+int n;
+cin>>n;
+vector<int> x;
+for(int i=0;i<n;i++){
+int y;
+cin>>y;
+x.push_back(y);
 
+}
+vector<int> ans=selection_sort(x);
 
 
 
